d7cca088f48118e5591fc6a1ed9b7ab2b10cf6fc
engr-kushagrachauhan <kushagra.chauhan_cs22@gla.ac.in>
2025-06-17 11:53:52 +0530

    Delete src directory

--- src/kush/high.java
+++
@@ -1,9 +0,0 @@
-package kush;
-public class high{
-	public static void main(String[] args) {
-		int a= 2000;
-		int b = 2024;
-		int c= a-b;
-		System.out.printf("harry's age in 2024 is = "+ c);
-	}
-}
--- src/kush/p2q3.java
+++
@@ -1,9 +0,0 @@
-package kush;
-public class p2q3{
-	public static void main(String[]args) {
-		double a = 10.6;
-		double b = 1.6;
-		double c= a*b;
-		System.out.printf("The distance"+a+"km in miles is"+c);
-	}
-}
