1d59405ad24a10a1348fa68e4676256cf1b6af58
engr-kushagrachauhan <kushagra.chauhan_cs22@gla.ac.in>
2025-06-25 11:55:34 +0530

    Add files via upload

---
+++ Java_Builtin_Function/FactorialRecursion.java
@@ -0,0 +1,19 @@
+package Java_Builtin_Function;
+
+import java.util.Scanner;
+
+public class FactorialRecursion {
+    static long factorial(int n) {
+        if (n <= 1) return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        System.out.println("Factorial of " + n + " is " + factorial(n));
+    }
+}
